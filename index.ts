3332995946d5d1e72e46232a54689df0f7ddab1b
IsmaHaa12 <tutaven2@gmail.com>
2025-12-04 09:18:08 +0700

    c

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
-// registerRootComponent calls AppRegistry.registerComponent
+// registerRootComponent calls
 registerRootComponent(App);
