bd7ca3966230fcb65a284c70e49ecb8927210639
IsmaHaa12 <tutaven2@gmail.com>
2025-12-12 07:25:25 +0700

    c

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
